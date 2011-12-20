41ad540ef9716de3aa6a9f87be32315f8e5d22f3
Jari Aalto <jari.aalto@cante.net>
2011-12-21 01:16:04 +0200

    emacs.bat: new

---
+++ emacs.bat
@@ -0,0 +1,9 @@
+@echo off
+
+REM http://www.emacswiki.org/emacs/EmacsClient
+REM
+REM “%~dp0” returns the directory in which the batch file resides,
+REM which is why it’s important to put it alongside “emacsclientw.exe”
+REM and “runemacs.exe”
+
+"%~dp0emacsclientw.exe" -na "%~dp0runemacs.exe" "%1"
